de9189f80b2fdeba5e278d1f30840493b3ef2c47
kathy95917 <kathy95917@naver.com>
2020-02-10 02:59:06 +0900

    로그인, 회원가입, 헤더 추가

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Home, Test } from "./pages";
+import Login from "./LoginOrSignup/Login";
+import Header from "./Common/header";
+import Signup from "./LoginOrSignup/Signup";
 import ErrorPage from "./pages/ErrorPage";
+import RegisterBook from "./Register/RegisterBook";
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/test" component={Test} />
+      <Route path="/login" component={Login} />
+      <Route path="/header" component={Header} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/registerBook" component={RegisterBook} />
       <Route component={ErrorPage} />
     </Switch>
   );
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,2 +1,6 @@
 export { default as Home } from "./Home";
 export { default as Test } from "./Test";
+export { default as Login } from "../LoginOrSignup/Login";
+export { default as Header } from "../Common/header";
+export { default as Signup } from "../LoginOrSignup/Signup";
+export { default as RegisterBook } from "../Register/RegisterBook";
