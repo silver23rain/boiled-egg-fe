10e67e558d895830c213bbe4b5d014ebf7eba0fb
kathy95917 <kathy95917@naver.com>
2020-02-19 18:04:53 +0900

    책 조회 목록 스타일 수정

--- src/components/Register/BooksearchList.jsx
+++ src/components/Register/BooksearchList.jsx
@@ -1,41 +1,108 @@
-import React,{Component} from 'react';
+import React,{Fragment} from 'react';
 import styled from 'styled-components';
+import font from '../../font/210옴니고딕040.ttf';
 
-
-
-  
 const BooksearchList = props => {
-    const books = props.books.map((book)=>{
+    const books = props.books.map((book, index)=>{
         return (
-            <div key={book.id}> 
-                <img src={book.thumbnail} alt="img"/>
-                <p>{book.title}</p>
-                <p>{book.publisher}</p>
-                <P>{book.publishedAt}</P>
-            </div>
-           
+            <Book key={index}> 
+                <Image src={book.thumbnail} alt="img"/>
+                <Title>{book.title}</Title>
+                <Author>{book.authors}지음 / {book.publisher}</Author>
+                <Publishat>{book.publishedAt}</Publishat>
+            </Book>
+
            
         );
     });
 
     return (
-        <Wrapper>
-            {books}
-        </Wrapper>
+        <Fragment>
+            <Wrapper>
+                {books}
+            </Wrapper>
+            <Total>총 {books.length}개의 결과</Total>
+        </Fragment>
     );
 }
-const P = styled.p`
-    display: inline-block; 
-    width: 100px; 
+const Total = styled.div`
+    position: absolute;
+    top: 1033.5px;
+    left: 363.5px;
+    width: 140px;
+    font-size: 20px;
+    color: #707070;
+    z-index:5;
+`; 
+const Book = styled.div`
+    height: 160px;
+    
+`;
+const Author = styled.div`
+    font-size: 22px;
+    color: #707070;
+    position: relative;
+    top: -66px;
+    left: 170px;
+    height: 318px;
+    width: 318px;
+    white-space: nowrap; 
+    overflow: hidden; 
+    text-overflow: ellipsis;
+    
+`
+const Title = styled.div`
+    @font-face {
+        font-family: '210옴니고딕040';
+        src: url(${font}) format('truetype');
+    }
+    font-size: 25px;
+    font-family: '210옴니고딕040';
+    width: 318px;
+    color: #000000;
+    position: relative;
+    top: -80px;
+    left: 170px;
+    white-space: nowrap; 
+    overflow: hidden; 
+    text-overflow: ellipsis;
+    `;
+
+const Image = styled.img`
+    position: relative;
+    top: 30px;
+    left: 34px;
+    height:106px;
+    width:106px;
+`;
+const Publishat = styled.div`
+    position: relative;
+    top: -345px;
+    left: 170px;
+    font-size: 22px;
+    color: #707070;
+    width: 90px; 
     white-space: nowrap; 
     overflow: hidden; 
     
 `;
 const Wrapper = styled.div`
     position: absolute;
-    top:30px;
-    left: 1167px;
-    z-index:4;
-
+    z-index:5;
+    height: 650px;
+    width: 540px;
+    overflow-x: hidden;
+    overflow-y: auto;
+    &::-webkit-scrollbar {
+    width: 16px;  /* 세로축 스크롤바 길이 */
+    height: 1080px;  /* 가로축 스크롤바 길이 */
+    background-color: #ffde2b;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: #f7f7f7;
+    height: 1080px;
+  }
+  
 `;
+
 export default BooksearchList;
